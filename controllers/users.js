5f15b78f2d60b1412e3da56ea1c162720df11b47
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-09-21 13:09:20 +0300

    add auth

---
+++ controllers/users.js
@@ -0,0 +1,68 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+const { HttpError, ctrlWrapper } = require("../helpers");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = process.env;
+
+const registration = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+  res.status(201).json({
+    user: { email: newUser.email, subscription: newUser.subscription },
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const payload = { id: user._id };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.json({
+    token,
+    user: { email: user.email, subscription: user.subscription },
+  });
+};
+
+const getCurrent = (req, res) => {
+  const { email, subscription } = req.user;
+  res.json({ email, subscription });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.status(204).json();
+};
+
+const updateUser = async (req, res) => {
+  const { _id } = req.user;
+  const user = await User.findByIdAndUpdate(_id, req.body, { new: true });
+  res.json({ email: user.email, subscription: user.subscription });
+};
+
+module.exports = {
+  registration: ctrlWrapper(registration),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  updateUser: ctrlWrapper(updateUser),
+};
